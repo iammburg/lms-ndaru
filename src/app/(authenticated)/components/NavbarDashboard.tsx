35a9f39e1d5f1527655157b0fae46ec52b22fa87
iammburg <ilhamprabowo009@gmail.com>
2025-09-14 22:19:45 +0700

    feat: refactor Dashboard page to include user greeting and course links, enhance layout with responsive design

--- src/app/(authenticated)/components/NavbarDashboard.tsx
+++ src/app/(authenticated)/components/NavbarDashboard.tsx
@@ -7,7 +7,7 @@ import LogoutButton from './LogoutButton'
 
 const NavbarDasboard = () => {
   return (
-    <div className="min-h-screen bg-muted">
+    <div className="bg-muted">
       <nav className="h-16 bg-background border-b">
         <div className="h-full flex items-center justify-between max-w-(--breakpoint-xl) mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center gap-8">
